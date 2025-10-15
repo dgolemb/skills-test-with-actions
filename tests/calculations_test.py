e4f305e27ad9a06b78d35a955cf7b164a2754dbe
Dominik <161048243+dgolemb@users.noreply.github.com>
2025-10-15 17:18:23 +0000

    Add new tests

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -79,3 +79,14 @@ def test_get_nth_fibonacci_ten():
 
     # Assert
     assert result == 55
+
+
+def test_get_nth_fibonacci_negative():
+    """Test with a negative n."""
+    # Arrange
+    n = -1
+
+    # Act & Assert
+    with pytest.raises(ValueError) as exc_info:
+        get_nth_fibonacci(n)
+    assert str(exc_info.value) == "n cannot be negative"
